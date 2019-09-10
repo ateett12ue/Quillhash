c9acd66f4e49b8dd996b4906ad30f3c07b1d6392
ateet <16ucs050@lnmiit.ac.in>
2019-09-10 12:07:12 +0530

    Edit task added

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class TodoItem extends Component {
   render() {
-    const { title, handleDelete } = this.props;
+    const { title, handleDelete, handleEdit } = this.props;
     return (
       <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
         <h6>{title}</h6>
@@ -10,7 +10,7 @@ export default class TodoItem extends Component {
           <span className="mx-1 text-warning">
             <i className="fas fa-check"></i>
           </span>
-          <span className="mx-1 text-success">
+          <span className="mx-1 text-success" onClick={handleEdit}>
             <i className="fas fa-pen"></i>
           </span>
           <span className="mx-1 text-danger" onClick={handleDelete}>
