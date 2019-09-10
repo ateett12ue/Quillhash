03736a3b19c5c87535930281ba0f92da25af7f8d
ateet <16ucs050@lnmiit.ac.in>
2019-09-10 06:44:19 +0530

    TodoList UI completed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,11 +3,17 @@ import TodoItem from "./TodoItem";
 export default class TodoList extends Component {
   render() {
     return (
-      <div>
-        Hello From TODO List
-        <br />
-        <TodoItem />
-      </div>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">
+          <TodoItem />
+        </h3>
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-capitalize mt-5"
+        >
+          clear list
+        </button>
+      </ul>
     );
   }
 }
