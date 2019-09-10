c310a55da6f2415e2e7e7f5b6c5e699d1107e085
ateet <16ucs050@lnmiit.ac.in>
2019-09-10 06:40:02 +0530

    TodoInput UI completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 function App() {
   return (
-    <div>
-      Hello from app component
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-8 mt-4">
+          <h3 className="text-capitalize text-center">todo input</h3>
+          <TodoInput />
+          <TodoList />
+        </div>
+      </div>
     </div>
   );
 }
